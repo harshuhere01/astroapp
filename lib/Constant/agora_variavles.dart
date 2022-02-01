e578aa494e260b4382099cc1ce7841ed9d10f118
Rugved Patel <sanadkat@pragetx.com>
2022-02-01 12:05:01 +0530

    constant class changes

---
+++ lib/Constant/agora_variavles.dart
@@ -0,0 +1,5 @@
+class Agora{
+   static String APP_ID = "a13acf33f91445c7a5b9bba985bcf373";
+   static String Token = "006a13acf33f91445c7a5b9bba985bcf373IAAeG8WyEwfHrJXyWLfbIAintZ6VHL/1o9IHEbKuffE+u/9TQsIAAAAAEADbqsx0RdX4YQEAAQB61fhh";
+
+}
